a4069a750dd4ea3a7baad4f0c5f6103a10ce528b
Albert Qerimi <qerimialbert@gmail.com>
2024-08-30 01:45:14 +0200

    add roboto font

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,8 @@
     <title>Mentor KFZ</title>
     <link href="/kfz-app/assets/css/bootstrap-min.css" rel="stylesheet">
     <link href="/kfz-app/assets/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -21,21 +23,22 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/kfz-app/index.php">Home</a>
+                    <a class="nav-link" href="/kfz-app/index.php">Startseite</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kfz-app/client/list_clients.php">Clients</a>
+                    <a class="nav-link" href="/kfz-app/client/list_clients.php">Kunden</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kfz-app/invoice/create_invoice.php">Create Invoice</a>
+                    <a class="nav-link" href="/kfz-app/invoice/create_invoice.php">Rechnung erstellen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kfz-app/invoice/list_invoices.php">Invoices</a>
+                    <a class="nav-link" href="/kfz-app/invoice/list_invoices.php">Rechnungen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kfz-app/product/list_products.php">Products</a>
+                    <a class="nav-link" href="/kfz-app/product/list_products.php">Produkte</a>
                 </li>
             </ul>
         </div>
+
     </nav>
     <div class="container mt-4">
