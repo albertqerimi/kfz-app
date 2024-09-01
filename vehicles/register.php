149e7df62e039d9c28f8ac85dfd2df5f6edd32dc
Albert Qerimi <qerimialbert@gmail.com>
2024-09-01 15:38:08 +0200

    rename auto with vehicle

---
+++ vehicles/register.php
@@ -0,0 +1,22 @@
+        <!-- Vehicle Details -->
+        <h4>Fahrzeugdetails</h4>
+        <div class="form-group">
+            <label for="vin_number">Fahrgestellnummer (VIN):</label>
+            <input type="text" class="form-control" id="vin_number" name="vin_number">
+        </div>
+        <div class="form-group">
+            <label for="brand">Marke:</label>
+            <input type="text" class="form-control" id="brand" name="brand">
+        </div>
+        <div class="form-group">
+            <label for="model">Modell:</label>
+            <input type="text" class="form-control" id="model" name="model">
+        </div>
+        <div class="form-group">
+            <label for="license_plate">Kennzeichen:</label>
+            <input type="text" class="form-control" id="license_plate" name="license_plate">
+        </div>
+        <div class="form-group">
+            <label for="tuv_date">TÜV Ablaufdatum:</label>
+            <input type="date" class="form-control" id="tuv_date" name="tuv_date">
+        </div>
