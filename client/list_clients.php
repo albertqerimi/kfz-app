1a3080858d92ac76e157164e003b19cda762ef97
Albert Qerimi <qerimialbert@gmail.com>
2024-09-08 16:13:10 +0200

    edit list clients with pagination

--- client/list_clients.php
+++ client/list_clients.php
@@ -48,7 +48,7 @@ $customer_result = $customer_stmt->get_result();
         <a href="/kfz-app/client/register_client.php" class="btn btn-primary">Kunde Registrieren</a>
     </form>
 
-    <table class="table mt-3">
+    <table class="table table-striped mt-3">
         <thead>
             <tr>
                 <th>Kundennummer</th>
@@ -61,57 +61,64 @@ $customer_result = $customer_stmt->get_result();
             </tr>
         </thead>
         <tbody>
-            <?php while ($customer = $customer_result->fetch_assoc()): ?>
+            <?php if ($customer_result->num_rows > 0): ?>
+                <?php while ($customer = $customer_result->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($customer['kundennummer']); ?></td>
+                        <td><?php echo htmlspecialchars($customer['name']); ?></td>
+                        <td><?php echo htmlspecialchars($customer['street'] . ' ' . $customer['house_number'] . ', ' . $customer['postal_code'] . ' ' . $customer['city']); ?></td>
+                        <td><?php echo htmlspecialchars($customer['email']); ?></td>
+                        <td><?php echo htmlspecialchars($customer['phone']); ?></td>
+                        <td>
+                            <a href="edit_client.php?client_id=<?php echo htmlspecialchars($customer['id']); ?>" class="btn btn-info btn-sm">Bearbeiten</a>
+                            <a href="../vehicles/list.php?client_id=<?php echo htmlspecialchars($customer['id']); ?>" class="btn btn-success btn-sm">Fahrzeuge anzeigen</a> 
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            <?php else: ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($customer['kundennummer']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['name']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['street'] . ' ' . $customer['house_number'] . ', ' . $customer['postal_code'] . ' ' . $customer['city']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['email']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['phone']); ?></td>
-                    <td>
-                        <a href="edit_client.php?client_id=<?php echo htmlspecialchars($customer['id']); ?>" class="btn btn-info btn-sm">Bearbeiten</a>
-                        <a href="../vehicles/list.php?client_id=<?php echo htmlspecialchars($customer['id']); ?>" class="btn btn-success btn-sm">Fahrzeuge anzeigen</a> 
-                    </td>
+                    <td colspan="6" class="text-center">Keine Kunden gefunden</td>
                 </tr>
-            <?php endwhile; ?>
+            <?php endif; ?>
+
         </tbody>
     </table>
 
     <!-- Pagination -->
     <nav aria-label="Page navigation">
-    <ul class="pagination justify-content-center">
-        <?php
-        $total_pages = ceil($total_customers / $items_per_page);
-        $max_links = 10; 
+        <ul class="pagination justify-content-center">
+            <?php
+            $total_pages = ceil($total_customers / $items_per_page);
+            $max_links = 10; 
 
-        // Display "First" link
-        if ($total_pages > 1) {
-            echo '<li class="page-item ' . ($page == 1 ? 'disabled' : '') . '"><a class="page-link" href="list_clients.php?page=1&search=' . urlencode($search_query) . '">Erste</a></li>';
-        }
-
-        $start_page = max(1, $page - floor($max_links / 2));
-        $end_page = min($total_pages, $page + floor($max_links / 2));
-
-        // Adjust the range if it is too close to the start or end
-        if ($end_page - $start_page + 1 < $max_links) {
-            if ($start_page == 1) {
-                $end_page = min($total_pages, $start_page + $max_links - 1);
-            } else {
-                $start_page = max(1, $end_page - $max_links + 1);
+            // Display "First" link
+            if ($total_pages > 1) {
+                echo '<li class="page-item ' . ($page == 1 ? 'disabled' : '') . '"><a class="page-link" href="list_clients.php?page=1&search=' . urlencode($search_query) . '">Erste</a></li>';
             }
-        }
 
-        for ($i = $start_page; $i <= $end_page; $i++) {
-            echo '<li class="page-item ' . ($i == $page ? 'active' : '') . '"><a class="page-link" href="list_clients.php?page=' . $i . '&search=' . urlencode($search_query) . '">' . $i . '</a></li>';
-        }
+            $start_page = max(1, $page - floor($max_links / 2));
+            $end_page = min($total_pages, $page + floor($max_links / 2));
 
-        // Display "Last" link
-        if ($total_pages > 1) {
-            echo '<li class="page-item ' . ($page == $total_pages ? 'disabled' : '') . '"><a class="page-link" href="list_clients.php?page=' . $total_pages . '&search=' . urlencode($search_query) . '">Letzte</a></li>';
-        }
-        ?>
-    </ul>
-</nav>
+            // Adjust the range if it is too close to the start or end
+            if ($end_page - $start_page + 1 < $max_links) {
+                if ($start_page == 1) {
+                    $end_page = min($total_pages, $start_page + $max_links - 1);
+                } else {
+                    $start_page = max(1, $end_page - $max_links + 1);
+                }
+            }
+
+            for ($i = $start_page; $i <= $end_page; $i++) {
+                echo '<li class="page-item ' . ($i == $page ? 'active' : '') . '"><a class="page-link" href="list_clients.php?page=' . $i . '&search=' . urlencode($search_query) . '">' . $i . '</a></li>';
+            }
+
+            // Display "Last" link
+            if ($total_pages > 1) {
+                echo '<li class="page-item ' . ($page == $total_pages ? 'disabled' : '') . '"><a class="page-link" href="list_clients.php?page=' . $total_pages . '&search=' . urlencode($search_query) . '">Letzte</a></li>';
+            }
+            ?>
+        </ul>
+    </nav>
 
 </div>
 
